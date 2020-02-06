8f6ec405cf50ac0e1cf2c3184eb5d7205d2db244
Renat Bagautdinov <bagautdinovren@gmail.com>
2020-02-06 22:22:32 +0700

    continue edit page for products

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -10,19 +10,19 @@
         @method('PUT')
         <div class="mb-3">
             <label for="validationCustom01">Название</label>
-            <input type="text" class="form-control" id="validationCustom01" value="{{ $product->name }}" name="customer_name" required>
+            <input type="text" class="form-control" id="validationCustom01" value="{{ $product->name }}" name="name" required>
             <div class="valid-feedback">@lang('checkout.valid_correct')</div>
             <div class="invalid-feedback">@lang('checkout.valid_incorrect')</div>
         </div>
 
         <div class="mb-3">
             <label for="validationTextarea">Описание</label>
-            <textarea class="form-control" id="validationTextarea">{{ $product->description }}</textarea>
+            <textarea class="form-control" id="validationTextarea" name="description">{{ $product->description }}</textarea>
         </div>
 
         <div class="mb-3">
             <label for="validationCustom01">Цена</label>
-            <input type="text" class="form-control" id="validationCustom01" value="{{ $product->price }}" name="customer_name" required>
+            <input type="text" class="form-control" id="validationCustom01" value="{{ $product->price }}" name="price" required>
             <div class="valid-feedback">@lang('checkout.valid_correct')</div>
             <div class="invalid-feedback">@lang('checkout.valid_incorrect')</div>
         </div>
@@ -30,7 +30,7 @@
         <div class="form-group">
             <label>Статус</label>
             <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input form-control" id="customControlValidation1" @if($product->status) checked @endif>
+                <input type="checkbox" class="custom-control-input form-control" id="customControlValidation1" name="status" @if($product->status) checked @endif>
                 <label class="custom-control-label" for="customControlValidation1">Активный</label>
                 <div class="invalid-feedback">Example invalid feedback text</div>
             </div>
@@ -38,7 +38,7 @@
 
         <div class="form-group">
             <label>Раздел</label>
-            <select class="custom-select" required>
+            <select class="custom-select" name="section" required>
                 @foreach($sections as $section)
                     <option value="{{ $section->id }}">{{ $section->name }}</option>
                 @endforeach
@@ -48,34 +48,34 @@
 
         <div class="form-group">
             <label>Детальное изображение</label>
-            <div class="img-container my-2">
+            <div class="img-container my-2 text-center" data-prop="detail_photo">
                 @if (!empty($product->detail_photo))
-                    <span class="img-list-descr">(Drag&amp;Drop) <br> Перетащите картинку</span>
-                    <input type="file" name="file" class="img-field">
-                @else
                     <div class="img-block">
-                        <img src="https://placehold.it/200x200">
+                        <img src="{{ Storage::url($product->detail_photo) }}" class="img-fluid">
                     </div>
                     <div class="img-edit-panel text-center">
                         <span class="img-del"><i class="fas fa-trash-alt"></i></span>
                     </div>
+                @else
+                    <span class="img-list-descr">(Drag&amp;Drop) <br> Перетащите картинку</span>
+                    <input type="file" name="detail_photo" class="img-field">
                 @endif
             </div>
         </div>
 
         <div class="form-group">
             <label>Изображение для анонса</label>
-            <div class="img-container my-2">
+            <div class="img-container my-2 text-center" data-prop="preview_photo">
                 @if (!empty($product->preview_photo))
                     <div class="img-block">
-                        <img src="{{ Storage::url($product->preview_photo) }}">
+                        <img src="{{ Storage::url($product->preview_photo) }}" class="img-fluid">
                     </div>
-                    <div class="img-edit-panel text-center">
+                    <div class="img-edit-panel">
                         <span class="img-del"><i class="fas fa-trash-alt"></i></span>
                     </div>
                 @else
                     <span class="img-list-descr">(Drag&amp;Drop) <br> Перетащите картинку</span>
-                    <input type="file" name="file" class="img-field">
+                    <input type="file" name="preview_photo" class="img-field">
                 @endif
             </div>
         </div>
@@ -90,6 +90,72 @@
 
 @section('js')
     <script>
-        console.log('test');
+        $(document).ready(function() {
+            $(".img-container").on('click', '.img-del', function(){
+                let imgContainer = $(this).closest('.img-container');
+
+                imgContainer.empty();
+                $('<span class="img-list-descr">(Drag&amp;Drop) <br> Перетащите картинку</span>').appendTo(imgContainer);
+
+                if (!imgContainer.find('.img-field').length)
+                {
+                    $('<input type="file" name="' + imgContainer.data('prop') + '" class="img-field">').appendTo(imgContainer);
+                    $('<input type="hidden" name="update_' + imgContainer.data("prop") + '">').appendTo(imgContainer);
+                }
+                else
+                {
+                    imgContainer.find('.img-field').show();
+                }
+
+            });
+
+            $(".img-container").on('change', '.img-field', function(){
+                displayImg(this.files[0], $(this).closest('.img-container'));
+            });
+
+            $('.img-container').bind({
+                dragenter: function() {
+                    return false;
+                },
+                dragover: function() {
+                    return false;
+                },
+                dragleave: function() {
+                    return false;
+                },
+                drop: function(e) {
+                    displayImg(e.originalEvent.dataTransfer.files[0], $(this).closest('.img-container'));
+
+                    return false;
+                }
+            });
+
+            function displayImg(file, imgContainer) {
+                let imageType = /image.*/;
+
+                if (!file.type.match(imageType))
+                    return true;
+
+                imgContainer.find('.img-list-descr').remove();
+                imgContainer.find('.img-field').hide();
+
+                let imgBlock = $('<div class="img-block"></div>').appendTo(imgContainer);
+                let img = $('<img class="img-fluid"/>').appendTo(imgBlock);
+                let imgEditPanel = $('<div class="img-edit-panel text-center"></div>').appendTo(imgContainer);
+
+                $('<span class="img-del"><i class="fas fa-trash-alt"></i></span>').appendTo(imgEditPanel);
+
+                let reader = new FileReader();
+
+                reader.onload = (function(aImg) {
+                    return function(e) {
+                        aImg.attr('src', e.target.result);
+                        aImg.css('height', 180);
+                    };
+                })(img);
+
+                reader.readAsDataURL(file);
+            }
+        });
     </script>
 @endsection
