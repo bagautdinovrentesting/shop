ee4cbddb66bb391616d69f0b317c34b4b60d19cf
Renat Bagautdinov <bagautdinovren@gmail.com>
2021-02-18 20:41:40 +0700

    last changes

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -129,7 +129,7 @@ class ProductController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|View
      */
     public function edit($id)
     {
